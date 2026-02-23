b8841a6724792b1518ab10760cbe8f38cfb97b9a
Rand <ota.rra.nnd@gmail.com>
2026-02-23 03:19:37 +0300

    Lab4

--- libraryproject/apps/bookmodule/urls.py
+++ libraryproject/apps/bookmodule/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+# urlpatterns = [
+#     path('', views.index),
+#     path('index/<int:val1>/', views.index),
+#     path('<int:bookId>', views.viewbook), # هذا الرابط يستقبل أي رقم بعد /books/
+# ]
+#------------------------------------
 urlpatterns = [
-    path('', views.index),
-    path('index/<int:val1>/', views.index),
-    path('<int:bookId>', views.viewbook), # هذا الرابط يستقبل أي رقم بعد /books/
+    path('', views.index, name= "books.index"),
+    path('list_books/', views.list_books, name= "books.list_books"),
+    path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
+    path('aboutus/', views.aboutus, name="books.aboutus"),
 ]
